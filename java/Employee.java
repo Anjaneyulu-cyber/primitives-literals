d338929934321eb9f09c88d89fbfedce831b8bf8
Maata Anjaneyulu <anjaneyulumaata658@gmail.com>
2024-07-24 14:58:23 +0530

    feat : Update

--- java/Employee.java
+++ java/Employee.java
@@ -1,2 +1,15 @@
-package com.fundamenatals.java;public class Employee {
+package com.fundamenatals.java;
+
+public class Employee {
+    public static void main(String[] args) {
+
+        int age = 25;
+        String job = "Java Developer";
+        long packagePer = 50_00_000;
+        /*
+        Here we can't use package as a variable because it's a keyword
+         */
+        System.out.println(age+" "+job+" "+packagePer);
+
+    }
 }
